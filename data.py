b034a795e14958aa5f5980d2923cccc0e7ec03c9
99263 <992635910@qq.com>
2018-09-29 23:36:14 +0800

    init

--- data.py
+++ data.py
@@ -23,83 +23,101 @@ MAX_QUES_LENGTH = 30
 MAX_PASSAGE_LENGTH = 300
 MAX_WORD_INDEX = 30000
 
-with open(PATH + TRAIN, 'r', encoding='utf-8') as f:
-    train = f.readlines()
-    train = np.array(train)
-    np.random.shuffle(train)
-    train = train.tolist()
-with open(PATH + TEST, 'r', encoding='utf-8') as f:
-    test = f.readlines()
-    test = np.array(test)
-    np.random.shuffle(test)
-    test = test.tolist()
-with open(PATH + VALIODATION, 'r', encoding='utf-8') as f:
-    validation = f.readlines()
-    validation = np.array(validation)
-    np.random.shuffle(validation)
-    validation = validation.tolist()
 
-all_words = {'\n'}
-alternatives = []
-passages = []
-querys = []
-a = 0
-for i in train:
-    i = json.loads(i)
-    alternative = [' '.join(jieba.cut(ii, cut_all=True, HMM=False)) for ii in i.get('alternatives').split('|')]
-    alternatives.append(alternative)
-    passage = ' '.join(jieba.cut(i.get('passage').replace(' ', ''), cut_all=True,
-                                 HMM=False)).replace('   ', ' ， ').replace('  ', ' 。 ')
-    passages.append(passage)
-
-    if len(passage.split(' ')) > 300:
-        trs = TextRank4Sentence()
-        trs.analyze(text=i.get('passage').replace(' ', ''), lower=True, source='all_filters')
-        passages.append(' '.join(
-            jieba.cut('。'.join([i.sentence for i in trs.get_key_sentences(1)])[0:300], cut_all=True,
-                      HMM=False)).replace('   ', ' ， ').replace('  ', ' 。 '))
-
-    query = ' '.join(jieba.cut(i.get('query').replace(' ', ''), cut_all=True,
-                               HMM=False)).replace('   ', ' ').replace('  ', '')
-    querys.append(query)
-
-    for ii in alternative:
-        ii = set(ii.split(' '))
-        all_words |= ii
-    all_words |= set(passage.split(' ')) | set(query.split(' '))
-
-token = text.Tokenizer()
-token.fit_on_texts(all_words)
-index2word = token.index_word
+def shuf():
+    with open(PATH + TRAIN, 'r', encoding='utf-8') as f:
+        train = f.readlines()
+        train = np.array(train)
+        np.random.shuffle(train)
+        train = train.tolist()
+    with open('train_shuffle', 'wb') as f:
+        pickle.dump(train, f)
+    with open(PATH + TEST, 'r', encoding='utf-8') as f:
+        test = f.readlines()
+        test = np.array(test)
+        np.random.shuffle(test)
+        test = test.tolist()
+    with open('test_shuffle', 'wb') as f:
+        pickle.dump(test, f)
+    with open(PATH + VALIODATION, 'r', encoding='utf-8') as f:
+        validation = f.readlines()
+        validation = np.array(validation)
+        np.random.shuffle(validation)
+        validation = validation.tolist()
+    with open('val_shuffle', 'wb') as f:
+        pickle.dump(validation, f)
 
 
-def load_vectors(fname='cc.zh.300.vec'):
-    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    data = dict()
-    for line in fin:
-        tokens = line.rstrip().split(' ')
-        data[tokens[0]] = np.array([float(i) for i in tokens[1:]])
-    return data
+def tr():
+    all_words = {'\n'}
+    alternatives = []
+    passages = []
+    querys = []
+    train = pickle.load(open('train_shuffle', 'rb'))
+    for i in train:
+        i = json.loads(i)
+        alternative = [' '.join(jieba.cut(ii, cut_all=True, HMM=False)) for ii in i.get('alternatives').split('|')]
+        alternatives.append(alternative)
+        passage = ' '.join(jieba.cut(i.get('passage').replace(' ', ''), cut_all=True,
+                                     HMM=False)).replace('   ', ' ， ').replace('  ', ' 。 ')
+        passages.append(passage)
+
+        if len(passage.split(' ')) > 300:
+            trs = TextRank4Sentence()
+            trs.analyze(text=i.get('passage').replace(' ', ''), lower=True, source='all_filters')
+            passages.append(' '.join(
+                jieba.cut('。'.join([i.sentence for i in trs.get_key_sentences(1)])[0:300], cut_all=True,
+                          HMM=False)).replace('   ', ' ， ').replace('  ', ' 。 '))
+
+        query = ' '.join(jieba.cut(i.get('query').replace(' ', ''), cut_all=True,
+                                   HMM=False)).replace('   ', ' ').replace('  ', '')
+        querys.append(query)
+
+        for ii in alternative:
+            ii = set(ii.split(' '))
+            all_words |= ii
+        all_words |= set(passage.split(' ')) | set(query.split(' '))
+
+    token = text.Tokenizer()
+    token.fit_on_texts(all_words)
+    index2word = token.index_word
+
+    def load_vectors(fname='cc.zh.300.vec'):
+        fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        data = dict()
+        for line in fin:
+            tokens = line.rstrip().split(' ')
+            data[tokens[0]] = np.array([float(i) for i in tokens[1:]])
+        return data
+
+    #  word_vector = load_vectors()
+
+    alternatives_idx = []
+    for i in alternatives:
+        alternatives_idx.append(
+            [sequence.pad_sequences(token.texts_to_sequences('\n'.join(i)), maxlen=MAX_TRI_AN_LENGTH)])
+
+    passages_idx = [sequence.pad_sequences(token.texts_to_sequences(ii), maxlen=MAX_PASSAGE_LENGTH).tolist() for ii in
+                    passages]
+
+    querys_idx = [sequence.pad_sequences(token.texts_to_sequences(ii), maxlen=MAX_QUES_LENGTH) for ii in querys]
+
+    with open('index2word.pick', 'wb') as f:
+        pickle.dump(index2word, f)
+        del index2word
+
+    with open('alts.pick', 'wb') as f:
+        pickle.dump(np.array(alternatives_idx), f)
+        del alternatives_idx
+
+    with open('query.pick', 'wb') as f:
+        pickle.dump(np.array(querys_idx), f)
+        del querys_idx
+
+    for iii in passages_idx:
+        with open('pasg', 'a+') as f:
+            f.write('%s\n' % iii)
 
 
-word_vector = load_vectors()
-
-alternatives_idx = []
-for i in alternatives:
-    alternatives_idx.append([sequence.pad_sequences(token.texts_to_sequences('\n'.join(i)), maxlen=MAX_TRI_AN_LENGTH)])
-
-passages_idx = [sequence.pad_sequences(token.texts_to_sequences(ii), maxlen=MAX_PASSAGE_LENGTH) for ii in passages]
-
-querys_idx = [sequence.pad_sequences(token.texts_to_sequences(ii), maxlen=MAX_QUES_LENGTH) for ii in querys]
-
-with open('index2word.pick', 'wb') as f:
-    pickle.dump(np.array(index2word), f)
-
-with open('alts.pick', 'wb') as f:
-    pickle.dump(np.array(alternatives_idx), f)
-
-with open('query.pick', 'wb') as f:
-    pickle.dump(np.array(querys_idx), f)
-
-with open('pasg.pick', 'wb') as f:
-    pickle.dump(np.array(passages_idx), f)
+if __name__ == '__main__':
+    tr()
